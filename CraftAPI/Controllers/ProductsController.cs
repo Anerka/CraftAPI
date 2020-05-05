89091aacc45010ff351409196ffe0ce8217543f2
Anders Kardell <anders.kardell@starrepublic.com>
2020-05-05 11:58:06 +0200

    Add project files.

---
+++ CraftAPI/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using CraftAPI.Models;
+using CraftAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CraftAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            this.ProductsService = productsService;
+        }
+
+        public JsonFileProductsService ProductsService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductsService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery]string productId,[FromQuery] int rating)
+        {
+            ProductsService.AddRating(productId, rating);
+
+            return Ok();
+        }
+    }
+}
---
+++ CraftAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CraftAPI.Models;
+using CraftAPI.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CraftAPI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private JsonFileProductsService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
